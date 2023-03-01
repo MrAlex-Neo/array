b4ebb360ae1198c0b87d39e4e6c689e97b9d991c
Aleksandr <125996874+MrAlex-Neo@users.noreply.github.com>
2023-03-01 23:47:40 +0500

    Add files via upload

---
+++ js/examples.js
@@ -0,0 +1,9 @@
+let name = prompt('Ваше имя:')
+let age = +prompt('Укажите ваш возраст:')
+let students = [
+        ['Арслан', 21],
+        ['Максим', 35],
+        ['Артем', 33],
+    ]
+students.push = [[name, age]]
+alert(students)
